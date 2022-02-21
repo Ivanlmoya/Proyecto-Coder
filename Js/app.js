4d416c6722273a8dc3f35920b4ee3f1b5a42d6be
Ivan Moya <ivanlmoya@gmail.com>
2022-02-21 18:41:41 -0300

    Se agrego js para mostrar y ocultar los divs

--- Js/app.js
+++ Js/app.js
@@ -5,3 +5,78 @@ $(document).ready(function () {
     });
 
 });
+
+
+var a = 1;
+function showHide(){
+    if(a==1)
+    {
+        document.getElementById("div1").style.display="block";
+        document.getElementById("div2").style.display="none";
+        document.getElementById("div3").style.display="none";
+        document.getElementById("div4").style.display="none";
+        return a=0;
+    }
+    else{
+        document.getElementById("div1").style.display="block";
+        document.getElementById("div2").style.display="none";
+        document.getElementById("div3").style.display="none";
+        document.getElementById("div4").style.display="none";
+        return a=1;
+    }
+}
+
+function showHide2(){
+    if(a==1)
+    {
+        document.getElementById("div1").style.display="none";
+        document.getElementById("div2").style.display="block";
+        document.getElementById("div3").style.display="none";
+        document.getElementById("div4").style.display="none";
+        return a=0 ;
+    }
+    else{
+        document.getElementById("div1").style.display="none";
+        document.getElementById("div2").style.display="block";
+        document.getElementById("div3").style.display="none";
+        document.getElementById("div4").style.display="none";
+        return a=1 ;
+    }
+}
+
+function showHide3(){
+    if(a==1)
+    {
+        document.getElementById("div1").style.display="none";
+        document.getElementById("div2").style.display="none";
+        document.getElementById("div3").style.display="block";
+        document.getElementById("div4").style.display="none";
+        return a=0;
+    }
+    else{
+        document.getElementById("div1").style.display="none";
+        document.getElementById("div2").style.display="none";
+        document.getElementById("div3").style.display="block";
+        document.getElementById("div4").style.display="none";
+        return a=1;
+    }
+}
+
+
+function showHide4(){
+    if(a==1)
+    {
+        document.getElementById("div1").style.display="none";
+        document.getElementById("div2").style.display="none";
+        document.getElementById("div3").style.display="none";
+        document.getElementById("div4").style.display="block";
+        return a=0 ;
+    }
+    else{
+        document.getElementById("div1").style.display="none";
+        document.getElementById("div2").style.display="none";
+        document.getElementById("div3").style.display="none";
+        document.getElementById("div4").style.display="block";
+        return a=1 ;
+    }
+}
